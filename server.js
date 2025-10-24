c475b1cf85fba3f447ea2a177c12927c64554770
heyitsharman <harmankaur0779@gmail.com>
2025-10-24 16:39:16 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+
+//App config
+const app = express();
+const port = process.env.PORT || 4000
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.get('/',(req,res)=>{
+  res.send('api working');
+})
+
+app.listen(port,()=>{
+    console.log('server started on PORT : '+ port);
+})
