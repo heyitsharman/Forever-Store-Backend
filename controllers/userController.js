a46e67722d3827cd93de1f5d9b001127729a212b
heyitsharman <harmankaur0779@gmail.com>
2025-10-25 08:32:35 +0530

    user controller and routes added

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+//route for user login
+const loginUser =async (req,res)=>{
+
+}
+
+
+//route for user registration
+const registerUser = async (req,res)=>{
+
+    res.json({message:" register api working"})
+}
+
+//route for admin login
+const adminLogin = async (req,res)=>{
+
+}
+
+export {loginUser,registerUser,adminLogin};
